afa33c1e4cbcd40736a21fac8e4aff38b39638c5
Generah Ben <generahben@gmail.com>
2022-06-16 00:51:00 +0100

    Add Api Version, Disable Default Redirection and Send Json Error

--- api/routes/api/v1.php
+++ api/routes/api/v1.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::fallback(fn () => response()->json(['error' => 'Unknown request'], 404));
