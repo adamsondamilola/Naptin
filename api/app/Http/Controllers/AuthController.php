0bf3667dc6b2ec5867b446b602d23ab04fe9513d
Generah Ben <generahben@gmail.com>
2022-06-21 17:34:52 +0100

    Add Test For TraineeRegistration

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TraineeRegistrationRequest;
+
+class AuthController extends Controller
+{
+    public function register(TraineeRegistrationRequest $request): void
+    {
+
+    }
+}
